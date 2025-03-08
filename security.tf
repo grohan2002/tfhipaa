859d7104488aff8d368b9bdeb4844245c44f0a36
grohan2002 <grohan2002@gmail.com>
2025-03-08 12:02:34 +0530

    moving the security file out.

--- security.tf
+++ security.tf
@@ -1,14 +1,14 @@
-resource "aws_config_configuration_recorder" "main" {
-  name     = "config-recorder"
-  role_arn = aws_iam_role.config_role.arn
-}
+# resource "aws_config_configuration_recorder" "main" {
+#   name     = "config-recorder"
+#   role_arn = aws_iam_role.config_role.arn
+# }
 
-resource "aws_guardduty_detector" "main" {
-  enable = true
-}
+# resource "aws_guardduty_detector" "main" {
+#   enable = true
+# }
 
-resource "aws_securityhub_account" "main" {}
+# resource "aws_securityhub_account" "main" {}
 
-resource "aws_securityhub_standards_subscription" "hipaa" {
-  standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
-}
+# resource "aws_securityhub_standards_subscription" "hipaa" {
+#   standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
+# }
