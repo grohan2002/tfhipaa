3b766f8869d18cf09fe62875d80a112114dd13f8
grohan2002 <grohan2002@gmail.com>
2025-03-08 11:58:50 +0530

    Just creating the network stack first

--- eks.tf
+++ eks.tf
@@ -1,51 +1,51 @@
 
-# 🔹 EKS Cluster
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "hipaa-eks"
-  cluster_version = "1.28"
+# # 🔹 EKS Cluster
+# module "eks" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   cluster_name    = "hipaa-eks"
+#   cluster_version = "1.28"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-  enable_irsa = true
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   enable_irsa = true
 
-  eks_managed_node_groups = {
-    workers = {
-      desired_capacity = 3
-      max_capacity     = 5
-      min_capacity     = 2
+#   eks_managed_node_groups = {
+#     workers = {
+#       desired_capacity = 3
+#       max_capacity     = 5x
+#       min_capacity     = 2
 
-      instance_types = ["m5.large"]
-    }
-  }
-}
+#       instance_types = ["m5.large"]
+#     }
+#   }
+# }
 
-# 🔹 IAM Role for EKS Nodes
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
+# # 🔹 IAM Role for EKS Nodes
+# resource "aws_iam_role" "eks_node_role" {
+#   name = "eks-node-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Principal = { Service = "ec2.amazonaws.com" }
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
-# 🔹 Attach Policies to IAM Role
-resource "aws_iam_role_policy_attachment" "eks_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
+# # 🔹 Attach Policies to IAM Role
+# resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+#   role       = aws_iam_role.eks_node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+#   role       = aws_iam_role.eks_node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_ssm_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+# resource "aws_iam_role_policy_attachment" "eks_ssm_policy" {
+#   role       = aws_iam_role.eks_node_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+# }
