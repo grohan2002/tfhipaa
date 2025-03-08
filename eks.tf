46a3193a9a6af43df48404ae65ccea5eaad7771e
grohan2002 <grohan2002@gmail.com>
2025-03-08 11:51:47 +0530

    add node pools

--- eks.tf
+++ eks.tf
@@ -4,6 +4,17 @@ module "mgmt_eks" {
   enable_fargate  = true
   vpc_id          = module.mgmt_vpc.vpc_id
   subnet_ids      = module.mgmt_vpc.private_subnets
+
+  node_groups = {
+    workers = {
+      desired_capacity = 2
+      max_capacity     = 4
+      min_capacity     = 2
+
+      instance_types = ["m5.large"]
+    }
+  }
+
 }
 
 module "dev_eks" {
@@ -12,6 +23,16 @@ module "dev_eks" {
   enable_fargate  = true
   vpc_id          = module.dev_vpc.vpc_id
   subnet_ids      = module.dev_vpc.private_subnets
+
+  node_groups = {
+    workers = {
+      desired_capacity = 2
+      max_capacity     = 4
+      min_capacity     = 2
+
+      instance_types = ["m5.large"]
+    }
+  }
 }
 
 module "staging_eks" {
@@ -20,6 +41,16 @@ module "staging_eks" {
   enable_fargate  = true
   vpc_id          = module.staging_vpc.vpc_id
   subnet_ids      = module.staging_vpc.private_subnets
+
+  node_groups = {
+    workers = {
+      desired_capacity = 2
+      max_capacity     = 4
+      min_capacity     = 2
+
+      instance_types = ["m5.large"]
+    }
+  }
 }
 
 module "prod_eks" {
@@ -28,4 +59,15 @@ module "prod_eks" {
   enable_fargate  = true
   vpc_id          = module.prod_vpc.vpc_id
   subnet_ids      = module.prod_vpc.private_subnets
+
+  node_groups = {
+    workers = {
+      desired_capacity = 2
+      max_capacity     = 4
+      min_capacity     = 2
+
+      instance_types = ["m5.large"]
+    }
+  }
+  
 }
